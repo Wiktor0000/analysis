ff12d2857dec7a1b8e8001c23f4d09e4752949aa
Wiktor Musiałowski <w.musialowski01@gmail.com>
2024-07-17 00:14:28 +0200

    Badanie dokładności, rozkład poszczególnych zmiennych

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 from pandas import DataFrame, Series
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #Wczytywanie danych
 df: DataFrame = pd.read_csv(r"C:\Users\wmusi\OneDrive\Pulpit\data\all_data_apartments.csv")
@@ -59,3 +60,63 @@ beta_0 = y_mean - (beta_1 * x_mean)
 
 #Wyznaczanie prostej regresji - metoda najmniejszych kwadratów (MNK)
 prosta = (beta_1 * x) + beta_0
+
+#Wykres danych i dopasowanej linii regresji.
+plt.figure(figsize=(10, 6))
+plt.scatter(x, y, color='blue', label='Dane rzeczywiste')
+plt.plot(x, prosta, color='red', label='Prosta regresji')
+plt.xlabel('Powierzchnia (metry kwadratowe)')
+plt.ylabel('Cena')
+plt.title('Regresja liniowa - Metoda najmniejszych kwadratów')
+plt.legend()
+plt.show()
+
+#Badanie dokładności.
+SSm = np.sum((prosta - y_mean) ** 2)
+SSt = np.sum((y - y_mean) ** 2)
+r2 = SSm / SSt
+print(f"{r2:.2f}")
+
+#Rozkład zmiennych.
+# Wykres rozkładu powierzchni (metry kwadratowe)
+plt.figure(figsize=(10, 6))
+sns.histplot(df['square_meters'], kde=True, bins=30)
+plt.xlabel('Powierzchnia (metry kwadratowe)')
+plt.ylabel('Liczba mieszkań')
+plt.title('Rozkład powierzchni mieszkań')
+plt.show()
+
+#Wykres rozkładu - liczba pokoi.
+plt.figure(figsize=(10, 6))
+sns.histplot(df["rooms"], kde = True, bins = 30)
+plt.xlabel("Ilość pokoi w mieszkaniu")
+plt.ylabel("Liczba mieszkań")
+plt.title("Rozkład zmiennych - liczba pokoi")
+plt.show()
+
+#Wykres rozkładu - punkty zainteresowania (poi)
+plt.figure(figsize = (10, 6))
+sns.histplot(df["poi_count"], kde = True, bins = 30)
+plt.xlabel("Liczba poi w okolicy")
+plt.ylabel(" ")
+plt.title("Punkty zainteresowania (poi)")
+plt.xticks(ticks = np.arange(0, df["poi_count"].max() + 1, 10))
+plt.show()
+
+#Wykres rozkładu - odległość od centrum
+plt.figure(figsize = (10, 6))
+sns.histplot(df["centre_distance"], kde = True, bins = 30)
+plt.xlabel("Odległość od centrum (km)")
+plt.ylabel("Liczba mieszkań")
+plt.title("Odległość od centrum")
+plt.yticks(ticks = np.arange(0, 17000, 5000))
+plt.show()
+
+count_elevator = df["has_elevator"].value_counts()
+count_parking_space = df["has_parking_space"].value_counts()
+count_security = df["has_security"].value_counts()
+count_storage_room = df["has_storage_room"].value_counts()
+print(f"Mieszkania, które posiadają windę: {count_elevator}")
+print(f"Mieszkania, które posiadają miejsce parkingowe: {count_parking_space}")
+print(f"Mieszkania, które posiadają ochronę: {count_security}")
+print(f"Mieszkania, które posiadają komórkę lokatorską: {count_storage_room}")
